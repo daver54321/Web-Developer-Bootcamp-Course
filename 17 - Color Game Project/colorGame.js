66d3d506d54e81e0fd51a6d3cb95b7c3c05fa4c7
Dave Prince <daverprince@gmail.com>
2019-10-02 09:47:34 -0500

    Finish logic for RGB calculation

--- 17 - Color Game Project/colorGame.js
+++ 17 - Color Game Project/colorGame.js
@@ -6,13 +6,20 @@ var lowerLimitArray;
 var upperLimitArray;
 
 SetDifficulty("Easy");
+
 var buttons = document.querySelectorAll(".difficulty");
 buttons.forEach(element => {
-    this.addEventListener("click", function () {
-        SetDifficulty(this.textContent);
+    element.addEventListener("click", function () {
+        SetDifficulty(element.textContent);
     })
 
 });
+
+for (let index = 0; index < squares.length; index++) {
+    squares[index].addEventListener("click", function () {
+        ClickSquare(index);
+    });
+}
 
 Reset();
 
@@ -20,12 +27,11 @@ function Reset() {
     //Populate array containing square values
     squareValues = new Array();
     for (let index = 0; index < 6; index++) {
-        const color = RandomColor();
+        const color = RandomColor(index);
         squareValues[index] = new Object();
-        squareValues[index].R = color.R;
-        squareValues[index].G = color.G;
-        squareValues[index].B = color.B;
-        squareValues[index].rgbText = "rgb(" + squareValues[index].R + ", " + squareValues[index].G + ", " + squareValues[index].B + ")";
+        squareValues[index].RangeArray = color.RangeArray;
+        squareValues[index].ValueArray = color.ValueArray;
+        squareValues[index].rgbText = "rgb(" + squareValues[index].ValueArray.R + ", " + squareValues[index].ValueArray.G + ", " + squareValues[index].ValueArray.B + ")";
         squareValues[index].Winner = false;
     }
 
@@ -36,43 +42,90 @@ function Reset() {
     //Manipulate DOM style.backgroundColor
     for (let index = 0; index < squares.length; index++) {
         squares[index].style.backgroundColor = squareValues[index].rgbText;
-        squares[index].addEventListener("click", function() {
-            ClickSquare(index);
-        });
     }
+
 
     document.getElementById("rgbTitle").textContent = squareValues[winner].rgbText;
 
 }
 
-function RandomColor() {
-    
+function RandomColor(index) {
+
     var finalAnswer = {
-        R: 0,
-        G: 0,
-        B: 0,
+        RangeArray: {
+            R: ['', ''],
+            B: ['', ''],
+            G: ['', '']
+        },
+        ValueArray: {
+            R: 0,
+            G: 0,
+            B: 0,
+        }
     };
-    for (let index = 0; index < 3; index++) {
-        // Uses 1, 2, or 4 sections of 0-255 colorspace, with an "lower" and
-        // "upper" bound. Adds "change" to lower bound---or subtracts "change"
-        // from upper bounds---to create RGB triplet that is easy, medium or hard
-        var arraySection = Math.floor(Math.random() * lowerLimitArray.length);
-        var change = Math.floor(Math.random() * 32);
-        if ((Math.random() * 2) < 1) {
-            //Starting from the "lower" end of a section of 0-255 colorspace
-            finalAnswer[Object.keys(finalAnswer)[index]] = lowerLimitArray[arraySection] + change;
-        } else {
-            // Starting from the "upper" end of a section of 0-255 colorspace
-            finalAnswer[Object.keys(finalAnswer)[index]] = upperLimitArray[arraySection] - change;
+
+    // Create RangeArray, confirming that it isn't the same as an existing color square's array
+    finalAnswer.RangeArray = CreateRangeArray(index);
+
+    // Now populate ValueArray with more randomized numbers based on RangeArray
+    for (const key in finalAnswer.RangeArray) {
+        if (finalAnswer.RangeArray.hasOwnProperty(key)) {
+            const color = finalAnswer.RangeArray[key];
+            var change = Math.floor(Math.random() * 32);
+            var section = color[0];
+            var range = color[1];
+
+            if (range === "lower") {
+                //Starting from the "lower" end of a section of colorspace...
+                finalAnswer.ValueArray[key] = lowerLimitArray[section] + change;
+            } else {
+                // ...or starting from the "upper" end of a section of colorspace section
+                finalAnswer.ValueArray[key] = upperLimitArray[section] - change;
+            }
         }
     }
+
     return finalAnswer;
 }
 
+function CreateRangeArray(index) {
+    var arrayObject = {
+        R: ['', ''],
+        B: ['', ''],
+        G: ['', '']
+    }
+
+    for (const key in arrayObject) {
+        if (arrayObject.hasOwnProperty(key)) {
+            const color = arrayObject[key];
+            
+            // Uses 1, 2, or 4 sections of 0-255 colorspace, with an "lower" and
+            // "upper" bound. Adds "change" to lower bound---or subtracts "change"
+            // from upper bounds---to create RGB triplet that is easy, medium or hard
+            var arraySection = Math.floor(Math.random() * lowerLimitArray.length);
+            var range;
+            if ((Math.random() * 2) < 1) {
+                range = "lower";
+            } else {
+                range = "upper";
+            }
+            color[0] = arraySection;
+            color[1] = range;
+        }
+    }
+
+    for (let k = 0; k < index; k++) {
+        if (_.isEqual(squareValues[k].RangeArray, arrayObject)) {
+            arrayObject = CreateRangeArray(index);
+        }
+    }
+
+    return arrayObject;
+}
+
 function ClickSquare(clickedSquare) {
-    if(squareValues[clickedSquare].Winner === true) {
-        //All the winning stuff here
-        alert("This is the winner!");
+    if (squareValues[clickedSquare].Winner === true) {
+        document.get
     } else {
         alert("This isn't the winner.");
     }
@@ -86,17 +139,20 @@ function SetDifficulty(setting) {
         document.getElementById("easyButton").classList.add("active");
         document.getElementById("mediumButton").classList.remove("active");
         document.getElementById("hardButton").classList.remove("active");
+        Reset();
     } else if (setting === "Medium") {
         lowerLimitArray = [0, 128];
         upperLimitArray = [127, 255];
         document.getElementById("easyButton").classList.remove("active");
         document.getElementById("mediumButton").classList.add("active");
         document.getElementById("hardButton").classList.remove("active");
+        Reset();
     } else if (setting === "Hard") {
         lowerLimitArray = [0, 64, 128, 192];
         upperLimitArray = [63, 127, 191, 255];
         document.getElementById("easyButton").classList.remove("active");
         document.getElementById("mediumButton").classList.remove("active");
         document.getElementById("hardButton").classList.add("active");
+        Reset();
     }
 }
